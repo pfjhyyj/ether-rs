13bbeae904a5a224275edb50056d0ded6f8a7a2d
chipunderblade <pfjhyyj@qq.com>
2024-05-03 10:33:51 +0800

    feat: add user schema

--- entity/src/user.rs
+++ entity/src/user.rs
@@ -1,3 +1,4 @@
+use chrono::FixedOffset;
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
@@ -6,6 +7,12 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i64,
     pub username: String,
+    pub password: String,
+    pub avatar: String,
+    pub nickname: String,
+    pub mobile: String,
+    pub created_at: chrono::DateTime<FixedOffset>,
+    pub updated_at: chrono::DateTime<FixedOffset>,
 }
 
 #[derive(Clone, Copy, Debug, EnumIter, DeriveRelation)]
--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -1,47 +1,70 @@
 use sea_orm_migration::prelude::*;
 
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    UserId,
+    Username,
+    Password,
+    Nickname,
+    Avatar,
+    Mobile,
+    Email,
+    Status,
+    CreatedAt,
+    UpdatedAt,
+}
+
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                        ColumnDef::new(User::UserId)
+                        .big_integer()
+                        .not_null()
+                        .auto_increment()
+                        .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
-                    .to_owned(),
+                    .col(ColumnDef::new(User::Username).string().not_null().unique_key())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Nickname).string().null())
+                    .col(ColumnDef::new(User::Avatar).string().null())
+                    .col(ColumnDef::new(User::Mobile).string().null().unique_key())
+                    .col(ColumnDef::new(User::Email).string().null().unique_key())
+                    .col(ColumnDef::new(User::Status).small_integer().not_null().default(0))
+                    .col(
+                        ColumnDef::new(User::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp())
+                    )
+                    .col(
+                        ColumnDef::new(User::UpdatedAt)
+                        .timestamp_with_time_zone()
+                        .not_null()
+                        .default(Expr::current_timestamp())
+                    )
+                    .to_owned()
             )
-            .await
+        .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .drop_table(
+                Table::drop()
+                    .table(User::Table)
+                    .to_owned()
+            )
+        .await
     }
-}
-
-#[derive(DeriveIden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
 }
