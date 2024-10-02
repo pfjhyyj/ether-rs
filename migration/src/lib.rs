03147b61ca6aa6de6f15bf1cd2415b2bbadfd855
chipunderblade <pfjhyyj@qq.com>
2024-10-02 11:12:04 +0800

    init project

---
+++ migration/src/lib.rs
@@ -0,0 +1,14 @@
+pub use sea_orm_migration::prelude::*;
+
+mod m20241002_020949_init_user;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(m20241002_020949_init_user::Migration),
+        ]
+    }
+}
---
+++ migration/src/m20241002_020949_init_user.rs
@@ -0,0 +1,85 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    UserId,
+    Username,
+    Password,
+    Nickname,
+    Avatar,
+    Mobile,
+    Email,
+    Status,
+    CreatedAt,
+    UpdatedAt,
+}
+
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::UserId)
+                        .big_integer()
+                        .not_null()
+                        .auto_increment()
+                        .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Username).string().not_null().unique_key())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Nickname).string().null())
+                    .col(ColumnDef::new(User::Avatar).string().null())
+                    .col(ColumnDef::new(User::Mobile).string().null().unique_key())
+                    .col(ColumnDef::new(User::Email).string().null().unique_key())
+                    .col(ColumnDef::new(User::Status).small_integer().not_null().default(0))
+                    .col(
+                        ColumnDef::new(User::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp())
+                    )
+                    .col(
+                        ColumnDef::new(User::UpdatedAt)
+                        .timestamp_with_time_zone()
+                        .not_null()
+                        .default(Expr::current_timestamp())
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        
+        let insert = Query::insert()
+            .into_table(User::Table)
+            .columns([User::UserId, User::Username, User::Password, User::Nickname, User::Status])
+            .values_panic([
+                0.into(),
+                "ether_admin".into(),
+                "$2b$12$T43BHiuqzvV8DpN5TClvIeBse2dk0PBO4G9WVnFU/lY7mwcHDmnFy".into(),
+                "ether admin".into(),
+                0.into(),
+            ])
+            .to_owned();
+        manager.exec_stmt(insert).await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .table(User::Table)
+                    .to_owned()
+            )
+        .await
+    }
+}
