098f8642466d4b9eca0f70a8e102dee18fba4553
chipunderblade <pfjhyyj@qq.com>
2024-10-04 01:23:38 +0800

    feat: add log

--- api/src/router/app.rs
+++ api/src/router/app.rs
@@ -1,4 +1,5 @@
-use axum::{routing::post, Router};
+use axum::{body::Body, http::Request, routing::post, Router};
+use tower_http::trace::TraceLayer;
 
 use crate::controller;
 
@@ -7,6 +8,23 @@ pub fn init() -> Router {
     .route("/login", post(controller::auth::login_by_username));
   
   let app = Router::new()
-    .nest("/api", auth);
+    .nest("/api", auth)
+    .layer(axum::middleware::from_fn(utils::middleware::cors::handle))
+    .layer(axum::middleware::from_fn(utils::middleware::log::handle))
+    .layer(
+      TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+        let req_id = match request
+            .headers()
+            .get("x-request-id")
+            .and_then(|value| value.to_str().ok())
+        {
+            Some(v) => v.to_string(),
+            None => String::from("unknown"),
+        };
+        tracing::error_span!("request_id", id = req_id)
+    }),
+    )
+    .layer(axum::middleware::from_fn(utils::middleware::req_id::handle));
+
   app
 }
--- utils/src/middleware/mod.rs
+++ utils/src/middleware/mod.rs
@@ -1,3 +1,4 @@
 pub mod req_id;
 pub mod jwt;
 pub mod cors;
+pub mod log;
