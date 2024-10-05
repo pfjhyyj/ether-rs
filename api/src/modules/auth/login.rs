4c1335d63173feefd3591a3253c3a2807c83aa52
chipunderblade <pfjhyyj@qq.com>
2024-10-05 12:10:53 +0800

    fix: fix login resp

--- api/src/modules/auth/login.rs
+++ api/src/modules/auth/login.rs
@@ -23,6 +23,7 @@ pub struct LoginByUserNameRequest {
 }
 
 #[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct LoginByUserNameResponse {
     pub access_token: String,
     pub expire_time: String,
