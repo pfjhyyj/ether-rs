e732d9cf4bb551e26c449e05b33258eba23879db
chipunderblade <pfjhyyj@qq.com>
2024-10-20 07:59:18 +0800

    feat: add menu entity && migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20241002_020949_init_user;
+mod m20241019_115953_init_menu;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20241002_020949_init_user::Migration)]
+        vec![
+            Box::new(m20241002_020949_init_user::Migration),
+            Box::new(m20241019_115953_init_menu::Migration),
+        ]
     }
 }
---
+++ migration/src/m20241019_115953_init_menu.rs
@@ -0,0 +1,93 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveIden)]
+enum Menu {
+    Table,
+    MenuId,
+    ParentId,
+    Name,
+    Type,
+    Icon,
+    Path,
+    Sort,
+    Extra,
+    CreatedAt,
+    UpdatedAt,
+}
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Menu::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Menu::MenuId)
+                            .big_integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Menu::ParentId)
+                            .big_integer()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Name)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Type)
+                            .integer()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Icon)
+                            .string()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Path)
+                            .string()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Sort)
+                            .integer()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::Extra)
+                            .json()
+                            .null()
+                    )
+                    .col(
+                        ColumnDef::new(Menu::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(Menu::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Menu::Table).to_owned())
+            .await
+    }
+}
