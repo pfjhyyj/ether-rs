3605401452764c09867027cb812138ad7f37f7d7
chipunderblade <pfjhyyj@qq.com>
2024-10-19 11:13:31 +0800

    fix: fix auth current

--- api/src/modules/auth/mod.rs
+++ api/src/modules/auth/mod.rs
@@ -10,11 +10,12 @@ pub fn get_open_router() -> Router {
     Router::new()
         .route("/login", post(login::login_by_username))
         .route("/register", post(register::register_by_username))
-        .route("/current", get(current::get_current_user))
-        .route("/current", put(current::update_current_user))
-        .route("/current/password", put(current_password::update_password))
 }
 
 pub fn get_router() -> Router {
-    Router::new().route("/logout", post(logout::logout))
+    Router::new()
+        .route("/logout", post(logout::logout))
+        .route("/current", get(current::get_current_user))
+        .route("/current", put(current::update_current_user))
+        .route("/current/password", put(current_password::update_password))
 }
