93e89ef9cf54cf1cf9852a469318803c5673f017
chipunderblade <pfjhyyj@qq.com>
2024-12-14 18:02:43 +0800

    fix: change main

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,9 +1,24 @@
 pub mod modules;
 
-pub async fn serve() {
+#[tokio::main]
+pub async fn start() -> std::io::Result<()> {
+    utils::env::load_config();
+    utils::logger::init();
+    utils::db::init_db().await;
+    utils::redis::init_redis();
+
     let port = utils::env::get_env::<String>("API_PORT");
     let address = format!("0.0.0.0:{}", port);
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
     axum::serve(listener, modules::get_router()).await.unwrap();
+    Ok(())
+}
+
+pub fn main() {
+    let result = start();
+
+    if let Some(err) = result.err() {
+        tracing::error!("Failed to start server: {:?}", err);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-#[tokio::main]
-async fn main() {
-    utils::env::load_config();
-    utils::logger::init();
-    utils::db::init_db().await;
-    utils::redis::init_redis();
-
-    api::serve().await;
+fn main() {
+    api::main();
 }
